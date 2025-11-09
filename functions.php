231a7d23b89ba7d4a9cadf333a056475049e4367
Safat Alnur <safat@macbookpro.home>
2025-11-08 20:42:27 -0500

    FEATURE: add menu items in admin panel and register multiple nav menus options. Also update main style to include theme domain

--- functions.php
+++ functions.php
@@ -12,4 +12,16 @@ function alnur_scripts() {
 
 add_action('wp_enqueue_scripts', 'alnur_scripts');
 
+/**
+ * Activate menus
+ */
+function custom_theme_setup() {
+    add_theme_support('menus');
+    register_nav_menus(array(
+        'primary' => __('Primary Menu', 'custom_theme'),
+        'footer'  => __('Footer Menu', 'custom_theme'),
+    ));
+}
+add_action('after_setup_theme', 'custom_theme_setup');
+
 ?>
