05e233072ca9bb606eda608cad77df4e56241e35
Safat Alnur <safat@macbookpro.home>
2025-11-10 22:09:34 -0500

    FEATURE: add post content loops for all pages

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php get_header(); ?>
-    <h1>Hello World</h1>
+    <?php if(have_posts()) : 
+        while(have_posts()) : the_post(); ?>
+            <h1><?php the_title();?></h1>
+            
+            <small>Posted on: <?php the_time('F j, Y');?> at <?php the_time('g:i a');?>, in <?php the_category();?></small>
+            <p><?php the_content();?></p>
+            <br/>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <h2>No Content found</h2>
+    <?php endif; ?>
 <?php get_footer(); ?>
