fbf31bd9b036e4aabf8439111f9f6a4b17f0e439
Safat Alnur <safat@macbookpro.home>
2025-11-15 22:06:30 -0500

    FEATURE: add page specific template and non-page specific template for user to choose

--- templates/template-no-title.php
+++ templates/template-no-title.php
@@ -1,8 +1,12 @@
+<?php
+/**
+ * Template Name: No Title Layout
+ */
+?>
 <?php get_header(); ?>
     <?php if(have_posts()) : 
         while(have_posts()) : the_post(); ?>
-            <h1><?php the_title();?></h1>
-            <h2>Hello World!!!!</h2>
+            <h2>This is using No Title Layout template</h2>
             <p><?php the_content();?></p>
         <?php endwhile; ?>
     <?php else : ?>
