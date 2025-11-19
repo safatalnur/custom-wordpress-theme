31dbde8f9e0e38b250fad7c264eec25612ec855b
Safat Alnur <safat@macbookpro.home>
2025-11-19 00:29:40 -0500

    CHORES: new template for home page

---
+++ front-page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+<div class="row px-4">
+    <div class="col-8">
+        <?php if(have_posts()) : 
+            while(have_posts()) : the_post(); 
+            // echo 'Post Format: '. get_post_format();
+            ?>
+                <?php the_content();?>
+            <?php endwhile; ?>
+        <?php else : ?>
+            <h2>No Content found</h2>
+        <?php endif; ?>
+    </div>
+    <div class="col-4">
+        <div class="sticky-top" style="top: 70px !important">
+            <?php get_sidebar();?>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
