ab4f824b2a96d1b81b93b3f27b6fea3517fa3910
Safat Alnur <safat@macbookpro.home>
2025-11-18 19:26:07 -0500

    CHORES: update homepage styling

--- templates/template-no-title.php
+++ templates/template-no-title.php
@@ -4,12 +4,16 @@
  */
 ?>
 <?php get_header(); ?>
-    <?php if(have_posts()) : 
-        while(have_posts()) : the_post(); ?>
-            <h2>This is using No Title Layout template</h2>
-            <p><?php the_content();?></p>
-        <?php endwhile; ?>
-    <?php else : ?>
-        <h2>No Content found</h2>
-    <?php endif; ?>
+<div class="row px-4">
+    <div class="col-12">
+        <?php if(have_posts()) : 
+            while(have_posts()) : the_post(); ?>
+                <h2>This is using No Title Layout template</h2>
+                <p><?php the_content();?></p>
+            <?php endwhile; ?>
+        <?php else : ?>
+            <h2>No Content found</h2>
+        <?php endif; ?>
+    </div>
+</div>
 <?php get_footer(); ?>
