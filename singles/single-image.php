1c7a2a0583e5d42785b30db2898627cda738c75a
Safat Alnur <safat@macbookpro.home>
2025-11-24 01:23:22 -0500

    FEATURE: add comment section for the blog posts

--- singles/single-image.php
+++ singles/single-image.php
@@ -7,7 +7,18 @@
                 <h2 class="container-xl text-white py-5"><?php the_title();?></h2>
             </div>
             <div class="container-xl py-3">
+                <small><?php the_time('F j, Y');?> at <?php the_time('g:i a');?>, in <?php the_category(' ');?> - <?php the_tags(); ?></small>
                 <p><?php the_content();?></p>
+            </div>
+            <hr>
+            <div class="text-center">
+                <?php 
+                    if(comments_open()) :
+                        comments_template();
+                    elseif( !comments_open()) :
+                        echo ('No Comments are allowed');
+                    endif;
+                ?>
             </div>
             <br/>
         </div>
